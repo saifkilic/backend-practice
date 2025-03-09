937ad7521ff44e553c8b1e245d52932306eff5a3
saifkilic <saifsultan5417@gmail.com>
2025-03-09 23:33:43 +0500

    profsetup

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -2,9 +2,8 @@ import {asyncHandler} from "../utils/asyncHandler.js";
 
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "User registered successfully"
-    });
+   const {fullName, email, username, password} = req.body;
+    console.log({"email" : email});
 });
 
 
